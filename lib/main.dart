84034022015751a454df40f1f99f03479d443adf
ppen <ppen5@yahoo.com>
2019-09-24 09:49:34 +0700

    Start Project

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:demoapi/screens/read_all_data.dart';
+import 'package:flutter/material.dart';
+
+//void main() => runApp(MyApp());
+
+main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ReadAllData()
+    );
+  }
+}
---
+++ lib/screens/read_all_data.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ReadAllData extends StatefulWidget {
+  @override
+  _ReadAllDataState createState() => _ReadAllDataState();
+}
+
+class _ReadAllDataState extends State<ReadAllData> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Read All data'),
+      ),body: Text('body'),
+    );
+  }
+}
